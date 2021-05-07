dc1530d3f4d137f8e9b04c69e1dc8aa6bd692520
DragoonKite <frank.depaolo@yahoo.com>
2021-05-06 20:52:56 -0500

    remove color change on links when hovered

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,10 +3,10 @@ import React from 'react';
 function Nav(props){
     return(
         <nav className='nav nav-masthead justify-content-center float-md-end'>
-            <a data-testid="about" className="nav-link active" aria-current='page' href="#about">About me</a>
-            <a data-testid="portfolio" className="nav-link" href="#portfolio">Portfolio</a>
-            <a data-testid="contact" className="nav-link" href="#contact">Contact</a>
-            <a data-testid="resume" className="nav-link" href="#resume">Resume</a>
+            <a data-testid="about" className="nav-link active unlink" aria-current='page' href="#about">About me</a>
+            <a data-testid="portfolio" className="nav-link unlink" href="#portfolio">Portfolio</a>
+            <a data-testid="contact" className="nav-link unlink" href="#contact">Contact</a>
+            <a data-testid="resume" className="nav-link unlink" href="#resume">Resume</a>
         </nav>
     );
 };
