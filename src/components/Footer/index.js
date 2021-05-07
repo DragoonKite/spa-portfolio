e7befb327dab4d2ec6d98e8fa969d004f44ff4e5
DragoonKite <frank.depaolo@yahoo.com>
2021-05-06 20:24:44 -0500

    footer formated and icons added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,19 +1,16 @@
-import React from 'react'
+import React from 'react';
+import {GoMarkGithub} from 'react-icons/go';
+import {GrLinkedin} from 'react-icons/gr';
+import {FaStackOverflow} from 'react-icons/fa'
 
 function Footer () {
     return(
-        <footer>
-            <ul clasName='flex-row'>
-                <li className='mx 1'>
-                    <a href='https://github.com/DragoonKite' target='_blank' rel="noreferrer">Github</a>
-                </li>
-                <li className='mx-1'>
-                    <a href='https://www.linkedin.com/in/frank-depaolo-b85043a3/' target='_blank' rel="noreferrer">LinkedIn</a>
-                </li>
-                <li className='mx-1'>
-                    <a href='https://stackexchange.com/users/16893266/dragoonkite' target='_blank' rel="noreferrer">Stack Overflow</a>
-                </li>
-            </ul>
+        <footer className='mt-auto text-white-50'>
+            <p>
+                <a href='https://github.com/DragoonKite'  className='text-white px-3' target='_blank' rel="noreferrer"><GoMarkGithub size={24} /></a>
+                <a href='https://www.linkedin.com/in/frank-depaolo-b85043a3/' className='text-white' target='_blank' rel="noreferrer"><GrLinkedin size={24}/></a>
+                <a href='https://stackexchange.com/users/16893266/dragoonkite' className='text-white px-3' target='_blank' rel="noreferrer"><FaStackOverflow size={24}/></a>
+            </p>
         </footer>
     );
 }
