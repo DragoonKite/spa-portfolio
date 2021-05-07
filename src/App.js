397dc2a40b9085726a167f256603fd8766301b93
DragoonKite <frank.depaolo@yahoo.com>
2021-05-06 19:27:06 -0500

    minor changes to navbar placement

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import './App.css';
 import Header from './components/Header'
 import Project from './components/Project'
 import Footer from './components/Footer'
+
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <main>
+        <section className='my-5'>
+            <h1 id='about'>Who am I?</h1>
+            <div className="my-2">
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+                </p>
+            </div>
+        </section>
+      </main>
       <Footer></Footer>
     </div>
   );
